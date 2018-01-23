5119ab7af63292c357a5622080f73f4c3339116b
Jurgen Voorneveld <jurgen@ngti.nl>
2018-01-23 12:15:56 +0100

    Added some more help in the readme

---
+++ src/test/java/tools/Serialize.java
@@ -0,0 +1,37 @@
+package tools;
+
+import simplexml.SimpleXml;
+import simplexml.model.XmlAttribute;
+import simplexml.model.XmlName;
+import simplexml.model.XmlNoExport;
+import simplexml.model.XmlTextNode;
+
+public class Serialize {
+
+    private static class Project {
+        @XmlAttribute
+        @XmlName("other")
+        public final String name;
+
+        @XmlTextNode
+        public final String text;
+
+        @XmlNoExport
+        public final String hidden;
+
+        private Project(final String name, final String text, final String hidden) {
+            this.name = name;
+            this.text = text;
+            this.hidden = hidden;
+        }
+    }
+
+    public static void main(final String... args) {
+        final Project project = new Project("test<>&\"'", "Just a project", "invisible");
+
+        final SimpleXml simple = new SimpleXml();
+        System.out.println(simple.toXml(project));
+
+    }
+
+}
